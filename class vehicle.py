a9f8c4451aab6a391b8bc6644d5adda01aa32b2d
playtime139 <ryankimungui@gmail.com>
2025-12-30 14:46:50 +1100

    class vehicle.py

---
+++ class vehicle.py
@@ -0,0 +1,9 @@
+class vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+modelX = vehicle(240, 18)
+
+print("Model max speed:", modelX.max_speed)
+print("MOdel mileage:", modelX.mileage)
